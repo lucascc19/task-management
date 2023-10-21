33e232c34cc1517711c19fc6ac8628f1b66787b3
Lucas Oliveira <lucasrodriguesmano@gmail.com>
2023-10-21 19:16:44 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Enums/StatusTarefa.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTarefa
+    {
+        [Description("A Fazer")]
+        ToDo = 1,
+        [Description("Em Andamento")]
+        Doing = 2,
+        [Description("Concluído")]
+        Done = 3
+    }
+}
---
+++ SistemaDeTarefas/Repositories/UserRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositories.Interfaces;
+
+namespace SistemaDeTarefas.Repositories
+{
+    public class UserRepository : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContext _dbContext;
+
+        public UserRepository(SistemaTarefasDBContext sistemaTarefasDBContext) 
+        {
+            _dbContext = sistemaTarefasDBContext;
+        }
+
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbContext.Usuarios.ToListAsync();
+        }
+
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            await _dbContext.AddAsync(usuario);
+            _dbContext.SaveChanges();
+
+            return usuario;
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+
+            if(usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID: {id} não foi encontrado no banco de dados.");
+            }
+
+            usuarioPorId.Name = usuario.Name;
+            usuarioPorId.Email = usuario.Email;
+
+            _dbContext.Usuarios.Update(usuarioPorId);
+            _dbContext.SaveChanges();
+
+            return usuarioPorId;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+
+            if (usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID: {id} não foi encontrado no banco de dados.");
+            }
+
+            _dbContext.Usuarios.Remove(usuarioPorId);
+            _dbContext.SaveChanges();
+
+            return true;
+        }
+    }
+}
